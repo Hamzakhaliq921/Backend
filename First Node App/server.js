f4d1c06ab5108881748cdc71c8cae446dfbfc4ee
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-23 10:40:59 -0600

    paractise about path module and core modules of node js

--- First Node App/server.js
+++ First Node App/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
-
+const path= require('path')
 const app = express() 
+
+console.log('Folder anmee:'+path.dirname(__filename));
+console.log('Folder anmee:'+path.dirname(__dirname));
+console.log('file anmee:'+path.basename(__filename));
+console.log('Parse:',path.parse(__filename));
+//join
+
 
 app.get('/', (req, res) => {
     res.send(`<h1>Hello Hamza This is Front Page</h1>
@@ -17,3 +24,5 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 
 })
+
+
