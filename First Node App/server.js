f47954766880861bc758267ff8f8582fab77fd85
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-05-01 23:32:31 -0600

    Logic of task api

--- First Node App/server.js
+++ First Node App/server.js
@@ -121,4 +121,14 @@
 
 
 
+const express = require('express');
+const app = express();
 
+app.use(express.json());
+
+const taskRoutes = require('./routes/tasks');
+app.use('/api/tasks', taskRoutes);
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
