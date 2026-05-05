8d9a65f09b220d57df6c1f0e8acf8760a4eeff7b
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-05-05 11:40:08 -0600

    theory of event module

---
+++ First Node App/Eventmodule.js
@@ -0,0 +1,8 @@
+// In Node.js, the Event Module (called events) is used to handle events—basically actions or occurrences like clicking a button, receiving data, or finishing a task.
+
+// Think of it like this:
+// 👉 One part of your program fires an event
+// 👉 Another part listens and reacts to it
+
+
+
